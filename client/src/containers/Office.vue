c70acd0b97c4d049f8fb6962e6ea65879ce647f0
Andre Kradolfer <narfdre@gmail.com>
2018-01-05 22:46:32 -0700

    adding office stuff

---
+++ client/src/containers/Office.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="facebook">
+    Office
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Facebook',
+  async beforeCreate() {
+    if (!this.$store.getters.isLoggedIn) {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
