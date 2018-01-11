fdf86aff972c574554f41d966ce94e2b3e121742
Dave Welch <david@davidwelch.co>
2018-01-10 23:36:21 -0700

    pray for me semour

--- client/config/uat.env.js
+++ client/config/uat.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"uat"',
-  API_URL: '"https://someaws.com"'
+  API_URL: '"https://gg32hfrcnd.execute-api.us-west-2.amazonaws.com/dev/"'
 })
