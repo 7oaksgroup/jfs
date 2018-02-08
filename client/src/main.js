2f3b9b16317b3dadab03beba4e169fbc9fdfa523
Andre Kradolfer <narfdre@gmail.com>
2018-02-07 19:04:06 -0700

    loading bugsnag before everything else

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,8 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import './middleware/bugsnag'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-// Middleware
-import './middleware/bugsnag'
 
 Vue.config.productionTip = false
 
